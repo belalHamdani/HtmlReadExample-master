c9bd09eff72a1ed2e7376506825ae628b2cda374
belalhamdani <belal_hamdani26@milton.edu>
2024-09-29 23:11:30 -0400

    Initial commit

---
+++ src/HtmlRead.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class HtmlRead {
+
+    public static void main(String[] args) {
+        HtmlRead html = new HtmlRead();
+    }
+
+    public HtmlRead() {
+
+        try {
+            System.out.println();
+            System.out.print("hello \n");
+            URL url = new URL("https://www.milton.edu/");
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(url.openStream())
+            );
+            System.out.println(reader);
+            String line;
+
+            while( (line = reader.readLine()) != null )  {
+
+                if(line.contains("https")) {
+                    int start = line.indexOf("https");
+                   // System.out.println(line);
+                    String link = line.substring(start);
+                    //int end = line.indexOf("\"");
+
+                    int end = link.indexOf("\"");
+                    if (end ==-1){
+                        end = link.indexOf("'");
+                    }
+
+                    if (end==-1){
+                        end = link.indexOf("--");
+                    }
+
+
+
+
+                    System.out.println(start + ","+end);
+                    System.out.println(line.substring(start, start+end));
+                }
+
+            }
+            reader.close();
+        } catch(Exception ex) {
+            System.out.println(ex);
+            System.out.println("double check your url and start again");
+        }
+
+    }
+
+}
